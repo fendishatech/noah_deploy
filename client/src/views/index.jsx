e988bee5df261f3a575843b5ddb80f0b734cbe41
fendishatech <liyukibir@gmail.com>
2023-04-27 05:12:03 -0700

    new client setup

---
+++ client/src/views/index.jsx
@@ -0,0 +1,6 @@
+import Login from "./auth/Login";
+import Register from "./auth/Register";
+import PublicLayout from "./public/layout/index";
+import Home from "./public/home/Home";
+
+export { Login, Register, PublicLayout, Home };
